adda95f6b3c5b99e2051846f5c349b5fbc939dfe
Dylan Hu <git@dylan.hu>
2023-07-01 17:26:03 -0400

    add canvas and sidebar

---
+++ components/scene/Scene.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import {
+  GizmoHelper,
+  GizmoViewport,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Grid } from "@react-three/drei";
+
+export default function Scene() {
+  return (
+    <Canvas className="flex flex-grow h-full">
+      <color attach="background" args={["black"]} />
+      <Grid infiniteGrid sectionColor="white" cellColor="white" />
+      <PerspectiveCamera makeDefault position={[5, 2, 5]} />
+      <OrbitControls enableDamping enablePan target={[0, 0, 0]} />
+      <GizmoHelper alignment="bottom-right">
+        <GizmoViewport
+          axisColors={["red", "green", "blue"]}
+          labelColor="white"
+        />
+      </GizmoHelper>
+    </Canvas>
+  );
+}
---
+++ components/sidebar/Sidebar.tsx
@@ -0,0 +1,3 @@
+export default function Sidebar() {
+  return <aside className="flex h-full basis-96">Sidebar</aside>;
+}
