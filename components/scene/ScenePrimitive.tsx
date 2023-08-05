4c754d28c71c0801b0fffe46118bf00f1d645408
obloomfield <obloomfield@brown.edu>
2023-08-05 11:35:33 -0700

    phong progress part 2

--- components/scene/ScenePrimitive.tsx
+++ components/scene/ScenePrimitive.tsx
@@ -2,6 +2,7 @@ import { Primitive } from "@/types/Scenefile";
 import { useMemo } from "react";
 import { Color } from "three";
 
+import useScenefile from "@/hooks/useScenefile";
 import { phongFragShader } from "@/shaders/phongFragmentShader";
 import { phongVertexShader } from "@/shaders/phongVertexShader";
 
@@ -26,6 +27,7 @@ export default function ScenePrimitive({
   primitive: Primitive;
 }) {
 
+  const { scenefile } = useScenefile();
 
   // the uniform does not update with the primitive controls...
   // TODO: find some way to hook on dispatch of the primitive update? 
@@ -35,7 +37,11 @@ export default function ScenePrimitive({
     let ambient = primitive.ambient ?? [0, 0, 0];
     let diffuse = primitive.diffuse ?? [0, 0, 0];
     let specular = primitive.specular ?? [0, 0, 0];
-    let shininess = primitive.shininess ?? 1;
+    let shininess = primitive.shininess ?? 1.0;
+    let ambientCoefficient = scenefile.globalData.ambientCoeff ?? 1.0;
+    let diffuseCoefficient = scenefile.globalData.diffuseCoeff ?? 1.0;
+    let specularCoefficient = scenefile.globalData.specularCoeff ?? 1.0;
+    let transparentCoefficient = scenefile.globalData.transparentCoeff ?? 1.0;
     // TODO: get all lights from the scene, and pass them to the shader
     // (how to do best if they are in different groups?)
 
@@ -44,8 +50,12 @@ export default function ScenePrimitive({
       diffuseColor: { value: new Color(diffuse[0], diffuse[1], diffuse[2]) },
       specularColor: { value: new Color(specular[0], specular[1], specular[2]) },
       shininess: { value: shininess },
+      ambientCoefficient: { value: ambientCoefficient },
+      diffuseCoefficient: { value: diffuseCoefficient },
+      specularCoefficient: { value: specularCoefficient },
+      transparentCoefficient: { value: transparentCoefficient },
     };
-  }, [primitive]);
+  }, [scenefile, primitive]);
 
   
   return (
--- shaders/phongFragmentShader.ts
+++ shaders/phongFragmentShader.ts
@@ -5,21 +5,30 @@ uniform vec3 ambientColor;
 uniform vec3 diffuseColor;
 uniform vec3 specularColor;
 uniform float shininess;
+
+uniform float ambientCoefficient;
+uniform float diffuseCoefficient;
+uniform float specularCoefficient;
+uniform float transparentCoefficient; // TODO: implement (would be hard?)
+
+// placeholder. TODO: add lighting
 uniform vec3 lightPosition;
+uniform int lightCount;
 
 varying vec3 vNormal;
 
 void main() {
+
   vec3 lightDirection = normalize(lightPosition - vNormal);
-  vec3 diffuse = max(dot(vNormal, lightDirection), 0.0) * diffuseColor;
+  vec3 diffuse = diffuseCoefficient * max(dot(vNormal, lightDirection), 0.0) * diffuseColor;
 
   vec3 viewDirection = normalize(-vNormal);
   vec3 reflectionDirection = reflect(-lightDirection, vNormal);
   float specularFactor = max(dot(reflectionDirection, viewDirection), 0.0);
-  vec3 specular = pow(specularFactor, shininess) * specularColor;
+  vec3 specular = specularCoefficient * pow(specularFactor, shininess) * specularColor;
 
-  vec3 resultColor = ambientColor + diffuse + specular;
-  gl_FragColor = vec4(resultColor, 1.0);
+  vec3 resultColor = ambientCoefficient * ambientColor + diffuse + specular;
+  // gl_FragColor = vec4(resultColor, 1.0);
   gl_FragColor = vec4(ambientColor,1.0);
   
 }
