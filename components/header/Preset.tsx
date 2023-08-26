419f070c1b7a06ca80c2b10779caee0c010f8f07
Nicholas Vadasz <nicholas_vadasz@brown.edu>
2023-08-26 01:06:25 -0400

    adds preset selection for files

---
+++ components/header/Preset.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import useScenefile from "@/hooks/useScenefile";
+
+export default function Preset() {
+  const { loadFile } = useScenefile();
+  const { scenefilePath } = useScenefile();
+  // @ts-ignore
+  let files = require.context("examples", true, /\.json$/);
+
+  const handleClick = (file: string) => {
+    const blob = new Blob([JSON.stringify(files(file), null, 2)], {
+      type: "application/json",
+    });
+    loadFile(blob as File);
+  };
+
+  return (
+    <div className="p-1">
+      <Select onValueChange={handleClick}>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder={scenefilePath ?? "No file loaded"} />
+        </SelectTrigger>
+        <SelectContent>
+          {files.keys().map((key: string) => {
+            if (key.includes("examples/")) return;
+            return (
+              <SelectItem key={key} value={key}>
+                {key.replace("./", "").replace(".json", "")}
+              </SelectItem>
+            );
+          })}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+}
--- components/header/controls/HeaderControls.tsx
+++ components/header/controls/HeaderControls.tsx
@@ -1,9 +1,12 @@
 import Download from "../Download";
+import Preset from "../Preset";
 import Upload from "../Upload";
+import useScenefile from "@/hooks/useScenefile";
 
 export default function HeaderControls() {
   return (
     <div className="flex flex-shrink-0 justify-between items-center">
+      <Preset />
       <Download />
       <Upload />
     </div>
